e671c465a9790007df073cdfd22b91b6c205e07c
huangjifan <1360915472@qq.com>
2022-01-12 10:16:30 +0800

    first commit test

---
+++ binocular_vision/demo/build/src/CMakeFiles/binocular_vision_demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../bin/binocular_vision_demo"
+  "../../bin/binocular_vision_demo.pdb"
+  "CMakeFiles/binocular_vision_demo.dir/binocular_vision_01.cpp.o"
+  "CMakeFiles/binocular_vision_demo.dir/binocular_vision_01.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/binocular_vision_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ binocular_vision/demo/build/src/CMakeFiles/binocular_vision_demo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for binocular_vision_demo.
+# This may be replaced when dependencies are built.
